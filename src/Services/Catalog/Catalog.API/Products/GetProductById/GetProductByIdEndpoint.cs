6eb639e447aba1d40eba2aec70ba2728e0b02284
freelanceJsg <freelanceJsg@outlook.com>
2025-06-12 10:29:45 -0500

    feat: GetProductById

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id:guid}", async (Guid id, ISender sender) =>
+            {
+                var query = new GetProductByIdQuery(id);
+                var result = await sender.Send(query);
+                var response = new GetProductByIdResponse(result.Product);
+                return Results.Ok(response);
+            })
+            .WithTags("Product")
+            .WithName("GetProductById")
+            .WithSummary("Get Products By Id")
+            .WithDescription("Get Products By Id")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest); ;
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.Products.GetProductById
+{
+
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdHandler
+        (IDocumentSession session , ILogger<GetProductByIdHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Retrieving product with ID: {ProductId}", query.Id);
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken) ?? throw new ProductNotFoundException(query.Id);
+            logger.LogInformation("Product retrieved");
+            return new GetProductByIdResult(product);
+        }
+    }
+
+}
